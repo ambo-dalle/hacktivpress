2804430204401ad69c65c13a735732fc0503e23d
ambodalle <ambodalle.st@gmail.com>
2017-07-20 13:35:38 +0700

    add models

---
+++ server/models/artikel_models.js
@@ -0,0 +1,14 @@
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+
+var artikelSchema = new Schema({
+  title:  String,
+  content: String,
+  category:   String,
+  author: { type : Schema.Types.ObjectId, ref : 'User' },
+  createdAt: Date
+});
+
+var Artikel = mongoose.model('Artikel', artikelSchema);
+
+module.exports = Artikel
